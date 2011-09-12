c2a7c40951ac327f0ea2e3ff24f79c4a924f574b
Travis Cardwell <travis.cardwell@yuzutechnology.com>
2011-09-12 10:01:40 +0900

    implement transpose myself

--- exercises/chapter06/solutions/Exercise10.hs
+++ exercises/chapter06/solutions/Exercise10.hs
@@ -1,5 +1,15 @@
 -- base
-import Data.List (transpose)
+--import Data.List (transpose)
+transpose :: [[a]] -> [[a]]
+transpose = transpose' [] []
+  where
+    transpose' :: [a] -> [[a]] -> [[a]] -> [[a]]
+    transpose' cur rest (xs:xss)
+      | null xs   = transpose' cur rest xss
+      | otherwise = transpose' (head xs : cur) (tail xs : rest) xss
+    transpose' cur rest _
+      | null rest = []
+      | otherwise = reverse cur : transpose' [] [] (reverse rest)
 
 slide :: Int -> [a] -> [[a]]
 slide w xs | w > 0     = transpose (slide' w xs)
